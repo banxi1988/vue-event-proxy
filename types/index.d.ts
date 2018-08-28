f0594f6b1a4636bbd11f928b1b50c0ef8e570853
banxi1988 <banxi1988@gmail.com>
2018-08-28 10:39:10 +0800

    fix types 更新 types 声明

---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+import Vue, { VueConstructor, PluginFunction } from "vue";
+import "./vue";
+export declare const eventBus: object & Record<never, any> & Vue;
+export default class VueEventBus {
+  static install: PluginFunction<never>;
+}
